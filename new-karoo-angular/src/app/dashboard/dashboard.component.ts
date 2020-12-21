40c3c168b628e2b21f963d48244904071e68742f
dsnpack <dsnpack@PAT2735.alterdata.matriz>
2020-12-21 19:47:25 -0300

    Styling and adding get in api on dashboard

--- new-karoo-angular/src/app/dashboard/dashboard.component.ts
+++ new-karoo-angular/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { item } from "../models/DashboardModels/item.model";
+import axios from 'axios';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  lista: item[] = []; 
+
   constructor() { }
 
   ngOnInit(): void {
+    this.ngGetApi();
+
   }
 
+  async ngGetApi() {
+    try {
+      let list = await axios.get("http://localhost:3333/karoo/destaques");
+      console.log(list);
+      this.lista = list.data;
+
+    } catch(Exception) {
+      console.log("Peguei nada não");
+      return;
+
+    }
+  }
 }
